9ac33dec2d7f37ec8ee67c85718987f9b77defc7
minhphuc605 <43328859+minhphuc605@users.noreply.github.com>
2018-11-14 11:51:25 +0700

    goku moi

---
+++ DoAnKhoaIT/DoAnKhoaIT/Areas/Guest/Controllers/DaoTaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnKhoaIT.Areas.Guest.Controllers
+{
+    public class DaoTaoController : Controller
+    {
+        // GET: Guest/DaoTao
+        public ActionResult TDDH()
+        {
+            return View();
+        }
+    }
+}
