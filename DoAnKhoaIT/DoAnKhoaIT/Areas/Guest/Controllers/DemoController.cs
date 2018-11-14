c18dcd99993e020b9ef1456383394855f6d28582
HuaHaSon <43328342+HuaHaSon@users.noreply.github.com>
2018-11-14 10:23:51 +0700

    HASO

---
+++ DoAnKhoaIT/DoAnKhoaIT/Areas/Guest/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnKhoaIT.Areas.Guest.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Guest/Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
